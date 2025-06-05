18df197cf20f9bb541d7c722dab4eff19b5a78ad
Lucas-Azedo <2007.lucas.azevedo@gmail.com>
2025-06-04 22:04:20 -0300

    Transaction Controller added (CRUD)

---
+++ src/main/java/com/example/FinanceControl/controller/TransactionController.java
@@ -0,0 +1,40 @@
+package com.example.FinanceControl.controller;
+
+import com.example.FinanceControl.dto.request.TransactionRequestDTO;
+import com.example.FinanceControl.dto.response.TransactionResponseDTO;
+import com.example.FinanceControl.service.transaction.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<TransactionResponseDTO> createTransaction(@RequestBody TransactionRequestDTO dto){
+        return ResponseEntity.ok(transactionService.createTransaction(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponseDTO>> getAllTransaction(){
+        return ResponseEntity.ok(transactionService.getAllTransaction());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionResponseDTO> getTransactionById(@PathVariable UUID id){
+        return ResponseEntity.ok(transactionService.getTransactionById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable UUID id){
+        transactionService.deleteTransaction(id);
+        return ResponseEntity.noContent().build();
+    }
+}
