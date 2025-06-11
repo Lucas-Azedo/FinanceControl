9ff58fcee6c41f1708fe19336f00e255bc964153
Lucas-Azedo <2007.lucas.azevedo@gmail.com>
2025-06-10 22:04:41 -0300

    Minor formatting changes

--- frontend/src/views/auth/SignIn.vue
+++ frontend/src/views/auth/SignIn.vue
@@ -1,12 +1,9 @@
 <template>
     <div id="login" class="all">
         <h1>SignIn</h1>
-        <input v-model="email" placeholder="e-mail" />
+        <input v-model="email" placeholder="e-mail" type="email" />
         <input v-model="password" placeholder="Password" type="password" />
         <button @click="signIn">Logar</button>
-    </div>
-
-    <div>
         <button @click="signUp">Cadastrar</button>
     </div>
 </template>
@@ -19,39 +16,36 @@ import { useNavigation } from '../../composables/useNavigation'
 const { setToken } = useAuth()
 const { redirect } = useNavigation()
 
-    const email = ref('')
-    const password = ref('')
+const email = ref('')
+const password = ref('')
 
-    async function signIn() {
-        try{
-            const response = await fetch('http://localhost:8080/auth/signin', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    email: email.value,
-                    password: password.value
-                })
-            })
+async function signIn() {
+    try {
+        const response = await fetch('http://localhost:8080/auth/signin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value
+            }),
+        })
 
         const data = await response.json();
 
         if (!response.ok) {
             console.error('Erro do backend:', data)
-            throw new Error(data.message || 'Erro ao cadastrar')
+            throw new Error(data.message || 'Erro ao fazer login')
         }
 
         setToken(data.token)
-        console.log("Usuario logado!", data)
-
         redirect('/dashboard')
-
-        }catch(error){
-            console.error('Erro no signIn:', error)
-        }
+    } catch (error) {
+        console.error('Erro no signIn:', error)
     }
+}
 
-    function signUp(){
-        redirect('/signup')
-    }
+function signUp() {
+    redirect('/signup')
+}
 
 </script>
